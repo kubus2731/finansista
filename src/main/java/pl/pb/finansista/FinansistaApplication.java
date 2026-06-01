01cd8dc3d480cf63031a5b2c4b33b78dd3fcd1ba
kubus2731 <kubamatusiewicz65@gmail.com>
2026-06-01 19:43:10 +0200

    Initial commit - Spring Boot and Docker setup

---
+++ src/main/java/pl/pb/finansista/FinansistaApplication.java
@@ -0,0 +1,13 @@
+package pl.pb.finansista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pb/finansista/FinansistaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pb.finansista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinansistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
