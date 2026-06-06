bf23e2632047dde006f6ef6b4ae9c730dbeb9134
Jakub Borkowski <trinek.kontakt@gmail.com>
2026-06-06 06:46:32 +0200

    chore: remove unused imports from UserRepository

--- src/main/java/pl/pb/finansista/user/repository/UserRepository.java
+++ src/main/java/pl/pb/finansista/user/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package pl.pb.finansista.user.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.pb.finansista.user.User;
 
 import java.util.Optional;
