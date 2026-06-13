0c31c571a1f52b95cacb35c2d972e262267a79ab
kubus2731 <kubamatusiewicz65@gmail.com>
2026-06-13 18:42:28 +0200

    checkpoint: dzialajacy pelny formularz przed refaktorem na REST

---
+++ src/main/resources/db/migration/V9__add_samorzad_doktorantow.sql
@@ -0,0 +1,5 @@
+-- V9: Samorząd Doktorantów PB jako jednostka (równolegle do Samorządu Studentów).
+-- SDPB jest pełnoprawnym wnioskodawcą wg Zarządzenia 13/2025, a brakowało go w słowniku.
+INSERT INTO department (id, name) VALUES (16, 'Samorząd Doktorantów PB');
+
+COMMIT;
