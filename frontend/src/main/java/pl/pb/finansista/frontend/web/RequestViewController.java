1700655b468fed32f0aa91a7017d91731f09a59c
Jakub Borkowski <trinek.kontakt@gmail.com>
2026-06-18 17:56:57 +0200

    fix(backend): remove externalId from RequestResponse

--- frontend/src/main/java/pl/pb/finansista/frontend/web/RequestViewController.java
+++ frontend/src/main/java/pl/pb/finansista/frontend/web/RequestViewController.java
@@ -517,7 +517,7 @@ public class RequestViewController {
 
     private RequestView toView(RequestResponse r) {
         return new RequestView(
-                r.externalId(),
+                r.id(),
                 r.title(),
                 r.amount(),
                 r.status(),
